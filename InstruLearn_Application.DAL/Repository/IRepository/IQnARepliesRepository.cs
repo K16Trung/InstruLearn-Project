e46f84906f33c31f028acb39500dd0e4d8b24e29
Nam Hoàng <h0angnnam1123@gmail.com>
2025-02-28 22:45:42 +0700

    controller_service_Q_F

---
+++ InstruLearn_Application.DAL/Repository/IRepository/IQnARepliesRepository.cs
@@ -0,0 +1,13 @@
+﻿using InstruLearn_Application.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.DAL.Repository.IRepository
+{
+    public interface IQnARepliesRepository : IGenericRepository<QnAReplies>
+    {
+    }
+}
