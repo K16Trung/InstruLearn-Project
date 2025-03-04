cc75dc93eb3248836e3ab3afb2cb06a78229386b
Lê Bá Trung <trunglbse160522@fpt.edu.vn>
2025-03-04 07:33:28 +0700

    Update

---
+++ InstruLearn_Application.BLL/Service/IService/IClassService.cs
@@ -0,0 +1,19 @@
+﻿using InstruLearn_Application.Model.Models.DTO;
+using InstruLearn_Application.Model.Models.DTO.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.BLL.Service.IService
+{
+    public interface IClassService
+    {
+        Task<ResponseDTO> GetAllClassAsync();
+        Task<ResponseDTO> GetClassByIdAsync (string id);
+        Task<ResponseDTO> AddClassAsync (CreateClassDTO createClassDTO);
+        Task<ResponseDTO> UpdateClassAsync (UpdateClassDTO updateClassDTO);
+        Task<ResponseDTO> DeleteClassAsync (string id);
+    }
+}
---
+++ InstruLearn_Application.Model/Models/DTO/ClassDay/UpdateClassDayDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Models.DTO.ClassDay
+{
+    class UpdateClassDayDTO
+    {
+    }
+}
