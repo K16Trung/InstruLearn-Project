565fe9a73d0bcb1923e00e9a89bb538fa00689c1
Lê Bá Trung <trunglbse160522@fpt.edu.vn>
2025-03-04 10:28:22 +0700

    fix data

---
+++ InstruLearn_Application.Model/Migrations/20250304032555_fixclassday.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixclassday : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
