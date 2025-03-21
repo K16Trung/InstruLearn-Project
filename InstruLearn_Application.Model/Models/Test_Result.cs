884b483fd67d9bf032bb5164da303da81dc10db6
Nam Hoàng <h0angnnam1123@gmail.com>
2025-03-21 20:06:38 +0700

    update

--- InstruLearn_Application.Model/Models/Test_Result.cs
+++ InstruLearn_Application.Model/Models/Test_Result.cs
@@ -14,15 +14,15 @@ namespace InstruLearn_Application.Model.Models
         public int TestResultId { get; set; }
         public int LearnerId { get; set; }
         public int TeacherId { get; set; }
-        public int MajorTestId { get; set; }
-        public int LearningRegisId { get; set; }
+        public int? MajorTestId { get; set; }
+        public int? LearningRegisId { get; set; }
         public TestResultStatus Status { get; set; }
         
 
         // Navigation properties
         public Learner Learner { get; set; }
         public Teacher Teacher { get; set; }
-        public MajorTest MajorTest { get; set; }
-        public Learning_Registration LearningRegistration { get; set; }
+        public MajorTest? MajorTest { get; set; }
+        public Learning_Registration? LearningRegistration { get; set; }
     }
 }
