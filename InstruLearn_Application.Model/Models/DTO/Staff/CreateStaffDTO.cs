ca6c6a97af31c1b038cc4e14c82f06c79d8018de
Trung <116988878+K16Trung@users.noreply.github.com>
2025-03-24 14:40:12 +0700

    Update field for Account

--- InstruLearn_Application.Model/Models/DTO/Staff/CreateStaffDTO.cs
+++ InstruLearn_Application.Model/Models/DTO/Staff/CreateStaffDTO.cs
@@ -12,5 +12,6 @@ namespace InstruLearn_Application.Model.Models.DTO.Staff
         public string Username { get; set; }
         public string Fullname { get; set; }
         public string Password { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
--- InstruLearn_Application.Model/Models/DTO/Staff/UpdateStaffDTO.cs
+++ InstruLearn_Application.Model/Models/DTO/Staff/UpdateStaffDTO.cs
@@ -11,5 +11,10 @@ namespace InstruLearn_Application.Model.Models.DTO.Staff
         public string Fullname { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Avatar { get; set; }
+        public DateOnly DateOfEmployment { get; set; }
     }
 }
