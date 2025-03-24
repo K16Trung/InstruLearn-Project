0c640c2a5e2a1de2d4172ac6b67bf91cdcd76845
Trung <116988878+K16Trung@users.noreply.github.com>
2025-03-24 15:00:44 +0700

    Update

---
+++ InstruLearn_Application.Model/Models/DTO/Schedule/CreateScheduleDTO.cs
@@ -0,0 +1,19 @@
+﻿using InstruLearn_Application.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Models.DTO.Schedule
+{
+    public class CreateScheduleDTO
+    {
+        public int? TeacherId { get; set; }
+        public int? LearnerId { get; set; }
+        public int LearningRegisId { get; set; }
+        public TimeOnly TimeStart { get; set; }
+        public TimeOnly TimeEnd { get; set; }
+        public ScheduleMode Mode { get; set; }
+    }
+}
