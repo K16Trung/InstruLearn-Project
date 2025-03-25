5d8cb2db9054ef3baccd4cff98685b28e033b600
Trung <116988878+K16Trung@users.noreply.github.com>
2025-03-25 14:48:49 +0700

    Add Status for teacher, teachermajor

---
+++ InstruLearn_Application.Model/Migrations/20250325074831_add_status.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_status : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "TeacherMajors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Majors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LearningRequest",
+                table: "Learning_Registrations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "TeacherMajors");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Majors");
+
+            migrationBuilder.DropColumn(
+                name: "LearningRequest",
+                table: "Learning_Registrations");
+        }
+    }
+}
