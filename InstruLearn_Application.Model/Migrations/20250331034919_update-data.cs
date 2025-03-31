6e1a962b855c2be36b667628b1a783b1514eeca4
Trung <116988878+K16Trung@users.noreply.github.com>
2025-03-31 10:50:47 +0700

    Update database

---
+++ InstruLearn_Application.Model/Migrations/20250331034919_update-data.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LevelId",
+                table: "Learning_Registrations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Learning_Registrations_LevelId",
+                table: "Learning_Registrations",
+                column: "LevelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Learning_Registrations_LevelAssigneds_LevelId",
+                table: "Learning_Registrations",
+                column: "LevelId",
+                principalTable: "LevelAssigneds",
+                principalColumn: "LevelId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Learning_Registrations_LevelAssigneds_LevelId",
+                table: "Learning_Registrations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Learning_Registrations_LevelId",
+                table: "Learning_Registrations");
+
+            migrationBuilder.DropColumn(
+                name: "LevelId",
+                table: "Learning_Registrations");
+        }
+    }
+}
