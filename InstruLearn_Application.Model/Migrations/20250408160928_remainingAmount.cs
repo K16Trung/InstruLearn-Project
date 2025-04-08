5c89f9abf4d9af671e6f5d6fac8c58a5299a2c1e
Nam Hoàng <h0angnnam1123@gmail.com>
2025-04-08 23:10:13 +0700

    update

---
+++ InstruLearn_Application.Model/Migrations/20250408160928_remainingAmount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class remainingAmount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "RemainingAmount",
+                table: "Learning_Registrations",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemainingAmount",
+                table: "Learning_Registrations");
+        }
+    }
+}
