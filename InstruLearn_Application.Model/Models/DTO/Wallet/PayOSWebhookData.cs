fadb04430286df209f7c8a8ac8459dca7e31bab9
Nam Hoàng <h0angnnam1123@gmail.com>
2025-04-08 20:00:08 +0700

    update

---
+++ InstruLearn_Application.Model/Models/DTO/Wallet/PayOSWebhookData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Models.DTO.Wallet
+{
+    public class PayOSWebhookData
+    {
+        public string TransactionId { get; set; }
+        public long OrderCode { get; set; }
+        public string Status { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
