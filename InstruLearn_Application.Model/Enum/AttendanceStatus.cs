454686db243759f1d18b00e058c70b52d0de1b73
Nam Hoàng <h0angnnam1123@gmail.com>
2025-04-09 13:51:57 +0700

    update

---
+++ InstruLearn_Application.Model/Enum/AttendanceStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Enum
+{
+    public enum AttendanceStatus
+    {
+        NotYet = 0,
+        Present = 1,
+        Absent = 2,
+    }
+}
--- InstruLearn_Application.Model/Models/Schedules.cs
+++ InstruLearn_Application.Model/Models/Schedules.cs
@@ -24,6 +24,7 @@ namespace InstruLearn_Application.Model.Models
         public TimeOnly TimeStart { get; set; }
         public TimeOnly TimeEnd { get; set; }
         public ScheduleMode Mode { get; set; }
+        public AttendanceStatus AttendanceStatus { get; set; } = AttendanceStatus.NotYet;
         public ICollection<ScheduleDays> ScheduleDays { get; set; }
         public ICollection<ClassDay> ClassDays { get; set; }
 
