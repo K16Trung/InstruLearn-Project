5f8cdcb3188195ce5597b04c81aee21784bcec64
Nam Hoàng <h0angnnam1123@gmail.com>
2025-04-18 15:26:16 +0700

    syllabus link db

---
+++ InstruLearn_Application.Model/Migrations/20250418082503_syllabusLink.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class syllabusLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SyllabusLink",
+                table: "LevelAssigneds",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SyllabusLink",
+                table: "LevelAssigneds");
+        }
+    }
+}
