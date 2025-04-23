de4ec053a666a2da209f53722e6f1e0df751b1d6
Nam Hoàng <h0angnnam1123@gmail.com>
2025-04-23 14:46:20 +0700

    send notification

---
+++ InstruLearn_Application.Model/Models/DTO/LearningRegisFeedbackAnswer/CompleteFeedbackDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Models.DTO.LearningRegisFeedbackAnswer
+{
+    public class CompleteFeedbackDTO
+    {
+        public int FeedbackId { get; set; }
+        public bool ContinueStudying { get; set; }
+    }
+}
--- InstruLearn_Application.Model/Models/DTO/LearningRegisFeedbackOption/LearningRegisFeedbackOptionDTO.cs
+++ InstruLearn_Application.Model/Models/DTO/LearningRegisFeedbackOption/LearningRegisFeedbackOptionDTO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace InstruLearn_Application.Model.Models.DTO.LearningRegisFeedbackOption
@@ -10,5 +11,8 @@ namespace InstruLearn_Application.Model.Models.DTO.LearningRegisFeedbackOption
     {
         public int OptionId { get; set; }
         public string OptionText { get; set; }
+        public int QuestionId { get; set; }
+        [JsonIgnore]
+        public LearningRegisFeedbackQuestion.LearningRegisFeedbackQuestionDTO Question { get; set; }
     }
 }
