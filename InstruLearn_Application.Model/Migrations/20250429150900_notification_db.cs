772cce568267d68afa094dc1aeaf7fab4d8b8ebd
Nam Hoàng <h0angnnam1123@gmail.com>
2025-04-29 22:09:24 +0700

    notification db

---
+++ InstruLearn_Application.Model/Migrations/20250429150900_notification_db.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class notification_db : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StaffNotifications",
+                columns: table => new
+                {
+                    NotificationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LearningRegisId = table.Column<int>(type: "int", nullable: true),
+                    LearnerId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaffNotifications", x => x.NotificationId);
+                    table.ForeignKey(
+                        name: "FK_StaffNotifications_Learners_LearnerId",
+                        column: x => x.LearnerId,
+                        principalTable: "Learners",
+                        principalColumn: "LearnerId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_StaffNotifications_Learning_Registrations_LearningRegisId",
+                        column: x => x.LearningRegisId,
+                        principalTable: "Learning_Registrations",
+                        principalColumn: "LearningRegisId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffNotifications_LearnerId",
+                table: "StaffNotifications",
+                column: "LearnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaffNotifications_LearningRegisId",
+                table: "StaffNotifications",
+                column: "LearningRegisId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StaffNotifications");
+        }
+    }
+}
