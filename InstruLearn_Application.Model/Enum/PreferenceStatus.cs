035fa4c8f914e800d083ea6e75fa8d836ab6628f
Nam Hoàng <h0angnnam1123@gmail.com>
2025-05-09 15:10:18 +0700

    update db

---
+++ InstruLearn_Application.Model/Enum/PreferenceStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Enum
+{
+    public enum PreferenceStatus
+    {
+        None = 0,
+        ChangeTeacher = 1,
+        MakeupClass = 2
+    }
+}
---
+++ InstruLearn_Application.Model/Migrations/20250509080941_preferenceStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class preferenceStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PreferenceStatus",
+                table: "Schedules",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferenceStatus",
+                table: "Schedules");
+        }
+    }
+}
