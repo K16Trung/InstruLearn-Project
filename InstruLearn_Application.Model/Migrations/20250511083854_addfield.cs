115598d8d030284f29e3516ac060b647f6582062
Trung <116988878+K16Trung@users.noreply.github.com>
2025-05-11 15:39:41 +0700

    Update

---
+++ InstruLearn_Application.Model/Migrations/20250511083854_addfield.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalComments",
+                table: "LearningRegisFeedbacks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TeacherChangeReason",
+                table: "LearningRegisFeedbacks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeacherChangeReason",
+                table: "LearningRegisFeedbacks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalComments",
+                table: "LearningRegisFeedbacks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
