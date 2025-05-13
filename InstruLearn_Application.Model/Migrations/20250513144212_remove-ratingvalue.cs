a7ada442909ebeb2bfbdb6d23574bee12953af36
Trung <116988878+K16Trung@users.noreply.github.com>
2025-05-13 21:42:28 +0700

    Update data

---
+++ InstruLearn_Application.Model/Migrations/20250513144212_remove-ratingvalue.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstruLearn_Application.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeratingvalue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RatingValue",
+                table: "TeacherEvaluationOptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RatingValue",
+                table: "TeacherEvaluationOptions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
