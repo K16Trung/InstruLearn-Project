07db820549cd5317035b711421bb5df63df708e3
Nam Hoàng <h0angnnam1123@gmail.com>
2025-05-19 17:19:26 +0700

    SelfAssessment_db

---
+++ InstruLearn_Application.Model/Models/DTO/SelfAssessment/CreateSelfAssessmentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Models.DTO.SelfAssessment
+{
+    public class CreateSelfAssessmentDTO
+    {
+        public string Description { get; set; }
+    }
+}
---
+++ InstruLearn_Application.Model/Models/SelfAssessment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstruLearn_Application.Model.Models
+{
+    public class SelfAssessment
+    {
+        [Key]
+        public int SelfAssessmentId { get; set; }
+        public string Description { get; set; }
+        public ICollection<Learning_Registration> LearningRegistrations { get; set; }
+    }
+}
